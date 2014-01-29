51b22387c3420bfcfe3ac1293739d5a8e93b3d30
Alan Wilson <alan.w.wilson@gmail.com>
2014-01-29 11:56:07 +0000

    Update setup and pip requirements

--- setup.sh
+++ setup.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install python-pip python-dev build-essential
+sudo apt-get install python-pip python-dev build-essential libxml2-dev libxslt-dev libmysqlclient-dev libssl-dev
+#sudo pip install virtualenv
 
 cp NewsSpectrum/example_runapp.py runapp.py
-mkworkspace PyNewsSpec
-pip install -r NewsSpectrum/pip_requirements.txt
-pip install gunicorn supervisor
+
+#virtualenv PyNewsSpec
+#source PyNewsSpec/bin/activate
+sudo pip install --allow-external mysql-connector-python --allow-insecure mysql-connector-python -r NewsSpectrum/pip_requirements.txt
+#sudo pip install --allow-external mysql-connector-python --allow-unverified mysql-connector-python -r NewsSpectrum/pip_requirements.txt
+git clone https://github.com/nltk/nltk_contrib.git
+pushd nltk_contrib
+sudo python setup.py install
+popd
+
 cat >simple.conf <<EOF
 [program:myserver]
 command gunicorn runapp:app -w 4 -b 0.0.0.0:80
