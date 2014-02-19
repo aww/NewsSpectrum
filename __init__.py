dc114b04e179113ec54a939cdae68df0f12bd4e0
Alan Wilson <alan.w.wilson@gmail.com>
2014-02-19 08:21:23 +0000

    Don't pre-quote example URLs (was this supposed to go into a previous commit?)

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 
 from flask import Flask
-from urllib import quote
 
 app = Flask(__name__)
 
 examples = [
-    (quote('http://www.nbcnews.com/business/worst-performing-most-minicars-fail-new-frontal-crash-test-2D11968197'),                 'Small car crashes'),
-    (quote('http://bleacherreport.com/articles/1931210-warren-buffet-will-pay-1-billion-to-fan-with-perfect-march-madness-bracket'), '$1 billion prediction'),
-    (quote('http://www.theverge.com/2014/1/21/5332658/pharaoh-senebkay-mummy-abydos-dynasty'),                                       'New mummy'),
+    ('http://www.latimes.com/business/autos/la-fi-hy-toyota-honda-fail-iihs-crash-tests-20140121,0,2230299.story',
+     'Small car crashes'),
+    ('http://bleacherreport.com/articles/1931210-warren-buffet-will-pay-1-billion-to-fan-with-perfect-march-madness-bracket',
+     '$1 billion prediction'),
+    ('http://www.techtimes.com/articles/2897/20140123/archaeologists-stumped-by-discovery-of-3600-year-old-pharaoh-woseribre-senebkay-tomb.htm',
+     'New mummy'),
     ]
 
 import NewsSpectrum.views
